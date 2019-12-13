cc3180d635c2917e513a6775f52847e4c6c59f4f
Jose-Marcio Martins da Cruz <Jose-Marcio.Martins@mines-paristech.fr>
2019-12-13 10:31:58 +0100

    Some 3D images added. A sample script on how to read 3D images.
     On branch develop
    
     Changes to be committed:
    	modified:   README.md
    	new file:   images/3D/balls.raw
    	new file:   images/3D/balls/Maxime-08-0001.tif
    	new file:   images/3D/balls/Maxime-08-0002.tif
    	new file:   images/3D/balls/Maxime-08-0003.tif
    	new file:   images/3D/balls/Maxime-08-0004.tif
    	new file:   images/3D/balls/Maxime-08-0005.tif
    	new file:   images/3D/balls/Maxime-08-0006.tif
    	new file:   images/3D/balls/Maxime-08-0007.tif
    	new file:   images/3D/balls/Maxime-08-0008.tif
    	new file:   images/3D/balls/Maxime-08-0009.tif
    	renamed:    images/3D/lena01.png -> images/3D/lena/lena01.png
    	renamed:    images/3D/lena02.png -> images/3D/lena/lena02.png
    	renamed:    images/3D/lena03.png -> images/3D/lena/lena03.png
    	renamed:    images/3D/lena04.png -> images/3D/lena/lena04.png
    	renamed:    images/3D/lena05.png -> images/3D/lena/lena05.png
    	renamed:    images/3D/lena06.png -> images/3D/lena/lena06.png
    	renamed:    images/3D/lena07.png -> images/3D/lena/lena07.png
    	renamed:    images/3D/lena08.png -> images/3D/lena/lena08.png
    	renamed:    images/3D/lena09.png -> images/3D/lena/lena09.png
    	new file:   images/3D/raw/balls-000.raw
    	new file:   images/3D/raw/balls-001.raw
    	new file:   images/3D/raw/balls-002.raw
    	new file:   images/3D/raw/balls-003.raw
    	new file:   images/3D/raw/balls-004.raw
    	new file:   images/3D/raw/balls-005.raw
    	new file:   images/3D/raw/balls-006.raw
    	new file:   images/3D/raw/balls-007.raw
    	new file:   images/3D/raw/balls-008.raw
    	new file:   samplePrograms/read3DSample.py

--- README.md
+++ README.md
@@ -8,12 +8,19 @@ Some goodies to help use Smil under Python
 * __smilGoodies__  - Python modules
 
   * __read3D__ : this module helps reading 3D images
-  * __mplGui__ : this module contains a Matplotlib based GUI
+  * __mplGui__ : this module contains a Matplotlib based GUI.
+    This module has two reasons to be :
+    * no dependency on Qt libraries;
+    * can be used with jupyter notebooks.
 
 * __samplePrograms__ - examples on how to use these goodies
 
 * __examples__ - smilPython examples
 
 * __images__ - some images to play with
+
+# read3D
+
+# mplGui
 
 
---
+++ samplePrograms/read3DSample.py
@@ -0,0 +1,35 @@
+
+import smilPython as sp
+#import smilGoodies as sg
+import read3D as sg
+#from read3D import *
+
+print("Read a stack of 2D tiff images")
+imm = sg.read3DStack("images/3D/balls", expr = "*")
+
+print("Read a stack of 2D png images")
+iml = sg.read3DStack("images/3D/lena",  fext = ".png") 
+
+print("Read a stack of raw images")
+imb = sg.read3DStack("images/3D/raw", width = 512, height = 512, TYPE = "UINT8", fext = ".raw")
+
+print("Read a 3D raw image")
+ims = sg.read3DImage("images/3D/balls.raw", width = 512, height = 512, depth = 512, TYPE = "UINT8")
+
+print("Read a 3D raw image")
+imt = sg.read3DImage("images/3D/balls.raw", width = 512, height = 512, depth = 512, TYPE = "UINT8")
+
+imm.show()
+imm.setName("Stack of 2D tiff images")
+iml.show()
+iml.setName("Stack of 2D png images")
+imb.show()
+imb.setName("Stack of 2D raw images")
+ims.show()
+ims.setName("Raw 3D image")
+imt.show()
+imt.setName("Raw 3D image")
+
+input("Enter to continue...")
+
+
